5fad899e422657c5609473b01c4afff05b3cc897
l0n3s0m3-w0lf13 <91900328+l0n3s0m3-w0lf13@users.noreply.github.com>
2023-06-23 13:01:09 +0200

    06: component props

--- js/components/AppButton.js
+++ js/components/AppButton.js
@@ -3,21 +3,29 @@ export default {
         <button
         	:disabled="processing"
         	@click="toggle"
-        	class="bg-gray-200 hover:bg-gray-400 border rounded px-5 py-2 m-2 disabled:cursor-not-allowed"
+        	:class="{
+                'border rounded px-5 py-2 m-2 disabled:cursor-not-allowed': true,
+                'bg-violet-200 hover:bg-violet-400': type === 'primary',
+                'bg-pink-200 hover:bg-pink-400': type === 'secondary',
+                'bg-gray-200 hover:bg-gray-400': type === 'muted',
+                'is-loading': processing
+            }"
         >
             <slot />
         </button>
     `,
 
-    data() {
-        return {
-            processing: false,
+    props: {
+        // Nombre del prop
+        type: {
+            // Declarar tipo y valor por defecto
+            type: String,
+            default: 'primary'
+        },
+
+        processing: {
+            type: Boolean,
+            default: false
         }
     },
-
-    methods: {
-        toggle() {
-            this.processing = !this.processing;
-        }
-    }
 };
